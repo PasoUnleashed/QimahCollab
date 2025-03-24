01bf01f5184de48a8c7c23860e65ac18848345a7
Faisal Alsajjan <fta.sajjan@gmail.com>
2025-03-24 22:41:15 +0300

    feat: created base structure

--- src/qimahcollab/QimahCollab.java
+++ src/qimahcollab/QimahCollab.java
@@ -14,7 +14,8 @@ public class QimahCollab {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        TextGenerator t1 = new QimahGenerator();
+        System.out.println(t1.generateText());
     }
     
 }
---
+++ src/qimahcollab/QimahGenerator.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package qimahcollab;
+
+/**
+ *
+ * @author User
+ */
+public class QimahGenerator extends TextGenerator{
+
+    @Override
+    public String generateText() {
+        return "Hello Qimah";
+    }
+    
+}
